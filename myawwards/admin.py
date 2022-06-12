5332ce9c84d6422bdfba1f406ed860747ae88518
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-12 21:11:36 +0300

    register the models

--- myawwards/admin.py
+++ myawwards/admin.py
@@ -1,3 +1,8 @@
+
+from .models import Profile, Project, Reviews
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Reviews)
